a5e51697719c8e3c2cb7c2b102a3621bc5ab3b0e
Wesley <wesleywang2@gmail.com>
2017-04-20 15:48:55 -0700

    Complete Project

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -56,20 +56,100 @@ def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_phone(phone_number)
+  phone_number.gsub!(/[^0-9]/, "")
+  phone_number_length = phone_number.length
+
+  if phone_number_length < 10 || phone_number_length > 11
+    "0000000000"
+  elsif phone_number_length == 11 && phone_number[0] == "1"
+    phone_number.slice!(0)
+  else
+    phone_number
+  end
+end
+
+def find_peak(counts)
+  counts.key(counts.values.max)
+end
+
+def add_to_counts(counts, item)
+  if counts[item]
+    counts[item] += 1
+  else
+    counts[item] = 1
+  end
+end
+
+def find_hour_of_day(reg_date)
+  date = DateTime.strptime(reg_date, "%m/%d/%y %H:%M")
+  date.hour
+end
+
+def find_day_of_week(reg_date)
+  date = DateTime.strptime(reg_date, "%m/%d/%y %H:%M")
+  date.cwday
+end
+
+def convert_day(weekday)
+  case weekday
+  when 1
+    "Monday"
+  when 2
+    "Tuesday"
+  when 3
+    "Wednesday"
+  when 4 
+    "Thursday"
+  when 5 
+    "Friday"
+  when 6
+    "Saturday"
+  when 7 
+    "Sunday"
+  end
+end
+
+contents = CSV.open 'event_attendees.csv', headers: true, \
+                                           header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
+hours = {}
+days = {}
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
+  phone_number = clean_phone(row[:homephone])
+
+  hour = find_hour_of_day(row[:regdate])
+  add_to_counts(hours, hour)
+
+  day = convert_day(find_day_of_week(row[:regdate]))
+  add_to_counts(days, day)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  save_thank_you_letters(id, form_letter)
 end
+
+peak_hour = find_peak(hours)
+peak_day = find_peak(days)
+
+puts "The peak registration hour was #{peak_hour}"
+puts "The peak registration day was #{peak_day}"
 
 
